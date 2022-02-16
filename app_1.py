89de920a5ac6fa3e2e4f873a75bc4ab5e87e8df6
Ritwick Halder <89910345+halderritwick8@users.noreply.github.com>
2022-02-16 08:25:19 +0530

    Update app_1.py

--- app_1.py
+++ app_1.py
@@ -91,8 +91,8 @@ def app():
     cap = cv2.VideoCapture(url)
 
     while run:
-        success, img = cap.read()
-        imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        ret, frame = cap.read()
+        imgS = cv2.resize(frame, (0, 0), None, 0.25, 0.25)
         imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
         facesCurFrame = face_recognition.face_locations(imgS)
@@ -107,23 +107,23 @@ def app():
                 name = classNames[matchIndex].upper()
                 y1, x2, y2, x1 = faceLoc
                 y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-                cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(frame, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+                cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
                 markAttendance(name)
             else:
                 name = "Not_allowed"
                 y1, x2, y2, x1 = faceLoc
                 y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-                cv2.rectangle(img, (x1,y1), (x2, y2), (0, 0, 255), 2)
-                cv2.putText(img, "Not Allowed", (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                cv2.rectangle(frame, (x1,y1), (x2, y2), (0, 0, 255), 2)
+                cv2.putText(frame, "Not Allowed", (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
 #                 mixer.music.load('alarm.mp3')
 #                 mixer.music.play()
                 playsound('alarm.mp3')
                 markNTA(name)
 
-        cv2.imshow('Webcam_1', img)
-        FRAME_WINDOW.image(img)
+        cv2.imshow('Webcam_1', frame)
+        FRAME_WINDOW.image(frame)
         
 
         k = cv2.waitKey(6) & 0xFF
